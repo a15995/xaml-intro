51f00b0f2ee13ea672893e291f09716beb4b4ddb
a15995 <123@123.dk>
2016-11-23 12:49:18 +0100

    Intro

---
+++ XamlSamples/XamlSamples/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamlSamples
+{
+    public partial class XamlPlusCodePage
+    {
+        public XamlPlusCodePage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender,
+                                    ValueChangedEventArgs args)
+        {
+
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
